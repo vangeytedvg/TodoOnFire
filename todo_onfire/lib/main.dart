b1ef74c39e0a68232f3a49fffd0055664a67149b
Danny Van Geyte <vangeytedvg@gmail.com>
2019-06-18 20:35:05 +0200

    Volledige source nu

--- todo_onfire/lib/main.dart
+++ todo_onfire/lib/main.dart
@@ -1,40 +1,22 @@
 import 'package:flutter/material.dart';
-
-import 'screens/loginpage.dart';
+ 
+//pages
 import 'screens/dashboard.dart';
-
+import 'screens/loginpage.dart';
+import 'screens/signuppage.dart';
+ 
 void main() => runApp(new MyApp());
-
+ 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new MyHomePage(),
-      routes:<String, WidgetBuilder> {
-        '/homepage' : (BuildContext context) => DashboardPage()
+      home: LoginPage(),
+      routes: <String, WidgetBuilder> {
+        '/landingpage': (BuildContext context)=> new MyApp(),
+        '/signup': (BuildContext context) => new SignupPage(),
+        '/homepage': (BuildContext context) => new DashboardPage()
       },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Todo On Fire'),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      body:  new Center(
-          child: LoginPage(),
-        ),
-      );
   }
 }
