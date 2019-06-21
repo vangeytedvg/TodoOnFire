dbb61303064485e6bc59e29047f98d79871cf3f0
Danny Van Geyte <vangeytedvg@gmail.com>
2019-06-21 17:14:40 +0200

    From within Android Studio

--- todo_onfire/lib/screens/loginpage.dart
+++ todo_onfire/lib/screens/loginpage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//Firebase Auth
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todo_onfire/services/track.dart';
 import 'package:provider/provider.dart';
